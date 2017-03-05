c39f6c3f5b95df0009dd5c5d25443fe16380a945
malancas <malancaster0@gmail.com>
2017-03-04 23:27:11 -0500

    Corrected Cypher syntax in sessions

--- testinsert.py
+++ testinsert.py
@@ -1,29 +1,16 @@
-#! /usr/bin/python3
 # examples: https://neo4j.com/developer/python
 from neo4j.v1 import GraphDatabase, basic_auth
 
-def acquireDriver(uris, authToken, config):
-    for uri in uris:
-	try:
-		return GraphDatabase.driver(uri, authToken, config)
-        except ServiceUnavailableException:
-		# This URI failed, so loop around again if we have another
-		print("try again")
-    raise ServiceUnavailableException("No valid database URI found")
-
-print("Starting...")
+print "Starting..."
 # Get the Python driver and create a session to issue Cypher queries
-#driver = GraphDatabase.driver("bolt://localhost:7687", auth=basic_auth("neo4j", "neo4j"))
-uri = "bolt://localhost:7687"
-driver = acquireDriver(["bolt://localhost:7687", "bolt://52.33.222.241:7687"], basic_auth("neo4j", "neo4j"))
+driver = GraphDatabase.driver("bolt://localhost:7687", auth=basic_auth("neo4j", "*"))
 session = driver.session()
 
-
 # Import the csv and read it into a vector
-session.run("LOAD CSV WITH HEADERS FROM {} AS csvline WITH line", {"https://docs.google.com/spreadsheets/d/1N2z21Gg79k6nnWjDBR7YBzecAkELa7ekWiGpYzORV3U/edit?usp=sharing"})
+session.run("LOAD CSV WITH HEADERS FROM {file} AS csvline FIELDTERMINATOR ':' CREATE (:testActivity { name: csvline[0], tag: csvline[1], borough: csvline[2], indoors: csvline[3]})", {"file" : "https://drive.google.com/open?id=0B9-NO7e2_MJ1bzVaTWphT0gtT2s"})
 
 # Create a new object for each line from the csv
-for line in csvline:
-    session.run("CREATE (a:testActivity { name: line.name, tag: line.tag, borough: line.borough, indoors/outdoors: line.indoors/outdoors })")
+session.run("CREATE (:testActivity { name: csvline[0], tag: csvline[1], borough: csvline[2], indoors: csvline[3]})")
 
+print "Closing"
 session.close()
