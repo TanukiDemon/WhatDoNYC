0909b24b9e6954884ce7d03a15c862a43693cd2c
Dina Mangialino <dmangialino@users.noreply.github.com>
2017-03-26 16:02:26 -0400

    Update spotlight.js

--- app/static/spotlight.js
+++ app/static/spotlight.js
@@ -1,3 +1,7 @@
 function spotHG() {
-	myWindow=window.open("HG.html", "_blank", "toolbar=no,scrollbars=no,resiable=no,top=100,left=300,width=800,height=600");
+	myWindow=window.open("HG.html", "_blank", "toolbar=no,scrollbars=no,resizable=no,top=100,left=300,width=800,height=600");
+}
+
+function spotBH() {
+	myWindow=window.open("BH.html", "_blank", "toolbar=no,scrollbars=no,resizable=no,top=100,left=300,width=800,height=600")
 }
