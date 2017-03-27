699d2b091d681ed7399c5c637b32f4ff33a7e783
malancas <malancaster0@gmail.com>
2017-03-26 20:38:37 -0400

    Moved routes to views.py

--- app/views.py
+++ app/views.py
@@ -1,65 +1,82 @@
 from neo4j.v1 import GraphDatabase
-from flask import render_template, redirect, flash, request
-from app import app
+from flask import render_template, redirect, flash, request, g, Blueprint
 import requests
-import configparser
 import os
-
-from flaskr import app
-from flask import render_template, g
+from .wdnyc import app
+from .forms import signupForm, loginForm, wouldYouRatherForm
 
 my_view = Blueprint('my_view', __name__)
 
-def findNewEvents(uname, thold):
-    username = uname
-    threshold = thold
+def checkIfUserExists(form):
+    session = models.get_session()
+    return (session.query(models.User).filter(and_(models.User.username == form.username, models.User.password == form.password)))
 
-    query = ('MATCH (a1:`Activity`)<-[:`Has_rated`]-(u1:`User` {username:{username}}) '
-    'WITH count(a1) as counta '
-    'MATCH (u2:`User`)-[r2:`Has_rated`]->(a1:`Activity`)<-[r1:`Has_rated`]-(u1:`User` {user_id:{user_id}}) '
-    'WHERE (NOT u2=u1) AND (abs(r2.rating - r1.rating) <= 1) '
-    'WITH u1, u2, tofloat(count(DISTINCT a1))/counta as sim '
-    'WHERE sim>{threshold} '
-
-    'MATCH (a:`Activity`)<-[r:`Has_rated`]-(u2) '
-    'WHERE (NOT (a)<-[:`Has_rated`]-(u1)) '
-    'RETURN DISTINCT a,tofloat(sum(r.rating)) as score ORDER BY score DESC ')
-
-    tx = graph.cypher.begin()
-    tx.append(query, {'username': username, 'threshold': threshold})
-    result = tx.commit()
-
-
-def startSession():
-    config = confiparser.ConfigParser()
-    fn = os.path.join(os.path.dirname(__file__), 'config.ini')
-    config.read('config.ini')
-    neo_pw = config['global']['neo4j_password']
-
-    uri = "bolt://localhost:7687"
-    driver = GraphDatabase.driver(uri, auth=("neo4j", neo_pw))
-    return driver.session()
 
 @app.route('/')
 def home():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+    return render_template('index.html', title='Welcome')
+
 
 @app.route('/index', methods=['GET'])
 def index():
     return render_template('index.html', title='Welcome')
 
-@app.route('/recommendations', methods=['GET'])
-def recommendations():
-    return render_template('recs.html', title='Recommendations')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = signupForm(request.form)
+    if form.validate_on_submit():
+        # Check if user is already in database
+        if (checkIfUserExists(form)):
+            # If so, return register.html again
+            return render_template('signup.html', form=form)
+
+        # Otheriswe, insert the user in mysql database and render survey.html
+        else:
+            newUser = models.User(username=form.username, password=form.password, email=form.email, firstName=form.name)
+            session.add(newUser)
+            session.commit()
+      
+            session['username'] = form.username
+            return redirect('/WouldYouRather')
+    return render_template('signup.html', title='Recommendations', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = loginForm(request.form)
+    if form.validate_on_submit() and checkIfUserExists(form):
+        session['username'] = form.username.data
+        return redirect('/WouldYouRather')
+    
+    return render_template('login.html', title="Login", form=form)
+
+
+@app.route('/wyr', methods=['POST'])
+def wyr():
+# Serve "Would You Rather" survey
+    form = wouldYouRatherForm(request.form)
+    if 'username' in session:
+        username = session['username'] # Get current user's username
+    else:
+        return render_template('login.html') # User not logged in
+    if form.validate_on_submit():
+        # Add user and their preferences to Neo4j database
+        session = startSession()
+        session.run("CREATE (a:User {username: {uname}, trait1: {t1}, "
+                    "trait2: {t2}, trait3 {t3}, trait4 {t4}})",
+                    {"uname": username, "t1": form.foodOrScience,
+                     "t2": form.artOrHistory, "t3": form.outdoorsOrSports,
+                     "t4": form.entertainmentOrMusic})
+        return redirect('/wyr')
+    return render_template('wyr.html')
+
 
 @app.route('/questions')
 def questions():
     return render_template('questions.html', title="Daily Questions")
 
+
 @app.route('/spotlight')
 def spotlight():
     return render_template('spotlight.html', title="Spotlight")
-
-@app.route('/survey')
-def survey():
-    return render_template('survey.html', title="Survey")
--- app/wdnyc.py
+++ app/wdnyc.py
@@ -1,9 +1,8 @@
 import configparser
 from os import path
 from neo4j.v1 import GraphDatabase
-from flask import Flask, render_template, redirect, flash, request, session
+from flask import Flask
 import sqlite3 as lite
-from .forms import signupForm, loginForm, wouldYouRatherForm
 from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
@@ -13,85 +12,14 @@ fn = path.join(path.dirname(__file__), 'config.ini')
 config.read(fn)
 app.secret_key = config.get('global', 'secret_key')
 
+from .views import *
+
 def startNeo4JSession(config):
     neo_pw = config['global']['neo4j_password']
     uri = "bolt://localhost:7687"
     driver = GraphDatabase.driver(uri, auth=("neo4j", neo_pw))
     return driver.session()
 
-def checkIfUserExists(form):
-    session = models.get_session()
-    return (session.query(models.User).filter(and_(models.User.username == form.username, models.User.password == form.password)))
-
-
-@app.route('/')
-def home():
-    return render_template('index.html', title='Welcome')
-
-
-@app.route('/index', methods=['GET'])
-def index():
-    return render_template('index.html', title='Welcome')
-
-
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    form = signupForm(request.form)
-    if form.validate_on_submit():
-        # Check if user is already in database
-        if (checkIfUserExists(form)):
-            # If so, return register.html again
-            return render_template('signup.html', form=form)
-
-        # Otheriswe, insert the user in mysql database and render survey.html
-        else:
-            newUser = models.User(username=form.username, password=form.password, email=form.email, firstName=form.name)
-            session.add(newUser)
-            session.commit()
-      
-            session['username'] = form.username
-            return redirect('/WouldYouRather')
-    return render_template('signup.html', title='Recommendations', form=form)
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = loginForm(request.form)
-    if form.validate_on_submit() and checkIfUserExists(form):
-        session['username'] = form.username.data
-        return redirect('/WouldYouRather')
-    
-    return render_template('login.html', title="Login", form=form)
-
-
-@app.route('/wyr', methods=['POST'])
-def wyr():
-# Serve "Would You Rather" survey
-    form = wouldYouRatherForm(request.form)
-    if 'username' in session:
-        username = session['username'] # Get current user's username
-    else:
-        return render_template('login.html') # User not logged in
-    if form.validate_on_submit():
-        # Add user and their preferences to Neo4j database
-        session = startSession()
-        session.run("CREATE (a:User {username: {uname}, trait1: {t1}, "
-                    "trait2: {t2}, trait3 {t3}, trait4 {t4}})",
-                    {"uname": username, "t1": form.foodOrScience,
-                     "t2": form.artOrHistory, "t3": form.outdoorsOrSports,
-                     "t4": form.entertainmentOrMusic})
-        return redirect('/wyr')
-    return render_template('wyr.html')
-
-
-@app.route('/questions')
-def questions():
-    return render_template('questions.html', title="Daily Questions")
-
-
-@app.route('/spotlight')
-def spotlight():
-    return render_template('spotlight.html', title="Spotlight")
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0')
