495d46cf5b4b45163e6e6c5f4372b91e83d4183b
Dina Mangialino <dmangialino@users.noreply.github.com>
2017-03-28 13:11:26 -0400

    Update recs.js

--- app/static/recs.js
+++ app/static/recs.js
@@ -1,3 +1,15 @@
 function single() {
 	myWindow=window.open("singlerec.html", "_blank", "toolbar=no,scrollbars=no,resizable=no,top=100,left=300,width=800,height=600");
 }
+
+function validate() {
+	var x = document.forms["feedback"]["rating"].value;
+	if (x == "") {
+		alert("Please rate the suggestion to continue. Feedback will make your recommendations better!");
+		return false;
+	} else {
+		window.close();
+	}
+}
+
+
