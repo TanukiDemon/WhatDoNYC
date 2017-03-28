d44c6181ede65d3c499d3e872fa5d56e6b8582e5
BrittSternberg <sternbergbrittany@gmail.com>
2017-03-28 13:27:08 -0400

    Add files via upload

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
+from werkzeug.security import generate_password_hash
 
 Base = declarative_base()
 engine = create_engine("sqlite:///users.db")
@@ -16,5 +17,19 @@ class User(Base): #inherits Base
     password = Column(String)
     email = Column(String)
     firstName = Column(String)
+
+    def __init__(self, username, passowrd, email, firstName):
+        self.username = username
+        self.set_password(password)
+        self.email = email
+        self.firstname = firstname
+
+    def set_password(self, password):
+        self.pw_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.pw_hash password)
+
+
     def __repr__(self):
         return '<User "%d">' % (self.id)
