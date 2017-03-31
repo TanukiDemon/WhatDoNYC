2c7413f090dbea4fdfde483a49abc13b37a0bc83
BrittSternberg <sternbergbrittany@gmail.com>
2017-03-31 14:40:20 -0400

    Add files via upload

--- app/forms.py
+++ app/forms.py
@@ -6,10 +6,15 @@ class signupForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=1, max=15)])
 
     password = PasswordField('password', validators=[InputRequired(), Length(min=1, max=15)])
-    
+
     email = StringField('Email Address', validators=[InputRequired(), Length(min=6, max=35)])
 
-    name = StringField('Email Address', validators=[Length(min=1, max=35)])
+    name = StringField('name', validators=[InputRequired(), Length(min=1, max=35)])
+
+    securityQanswer = StringField('securityQanswer', validators=[InputRequired(), Length(min=1, max=35)])
+
+    securityQ = StringField('securityQ', validators=[InputRequired(), Length(min=1,max=35)])
+
 
 class loginForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=1, max=15)])
@@ -18,6 +23,9 @@ class loginForm(FlaskForm):
 
 class wouldYouRatherForm(FlaskForm):
     foodOrScience = RadioField('foodVSsci', choices = [('food', 'Food/Drink'), ('sci', 'Science')])
+
     artOrHistory = RadioField('artVShistory', choices = [('art', 'Arts'), ('history', 'History')])
+
     outdoorsOrSports = RadioField('outVSsports', choices=[('out', 'Outdoor'), ('sports', 'Sports')])
+
     entertainmentOrMusic = RadioField('entVSmusic', choices=[('ent', 'Entertainment'), ('music', 'Music')])
