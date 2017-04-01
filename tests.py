bc9a67f2bd9e32351d450569f4964ede1b698f0c
malancas <malancaster0@gmail.com>
2017-04-01 17:04:35 -0400

    Updated login tests. Added signup tests

--- tests.py
+++ tests.py
@@ -6,14 +6,37 @@ import tempfile
 class FlaskrTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
-        self.app = flaskr.app.test_client()
-        flaskr.init_db()
+        self.app.config['DATABASE'] = tempfile.mkstemp()
+        self.app = app.test_client()
 
     def tearDown(self):
-        os.close(self.db_fd)
-        os.unlink(flaskr.app.config['DATABASE'])
+        os.unlink(app.config['DATABASE'])
 
-    def test_empty_db(self):
-        rv = self.app.get('/')
-        assert b'No entries here so far' in rv.data
+    def login(self, username, password):
+        return self.app.post('/login', data=dict(
+            username=username,
+            password=password
+        ), follow_redirects=True)
+
+    def test_login(self):
+        rv = self.login('test', 'user')
+        assert b'You were logged in' in rv.data
+        rv = self.login('tests', 'user')
+        assert b'Invalid username' in rv.data
+        rv = self.login('test', 'users')
+        assert b'Invalid password' in rv.data
+
+    def signup(self, username, password, email, name, securityQ, securityA):
+        return self.app.post('/signup', data=dict(
+            username=username,
+            passwor=password,
+            email=email,
+            name=name
+            securityQ=securityQ
+            securityQanswer=securityA
+        ), follow_redirects=True)
+
+    def test_signup(self):
+        rv = self.signup('new', 'user', 'fake@com', 'fakename', 'fakeQ', 'fakeA')
+        assert b'You were registered' in rv.data
+        # Remove user from database here
