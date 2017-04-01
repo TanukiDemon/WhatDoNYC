2ccfdf6ede9df71db9feaa54198332c24030ee72
malancas <malancaster0@gmail.com>
2017-04-01 10:56:05 -0400

    Added testing file

---
+++ app/main_tests.py
@@ -0,0 +1,19 @@
+import os
+from .wdnyc import *
+import unittest
+import tempfile
+
+class FlaskrTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
+        self.app = flaskr.app.test_client()
+        flaskr.init_db()
+
+    def tearDown(self):
+        os.close(self.db_fd)
+        os.unlink(flaskr.app.config['DATABASE'])
+
+    def test_empty_db(self):
+        rv = self.app.get('/')
+        assert b'No entries here so far' in rv.data
