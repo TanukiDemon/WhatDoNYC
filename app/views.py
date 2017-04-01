d232c235df9b10c96a554212b3905c72f69c29cd
BrittSternberg <sternbergbrittany@gmail.com>
2017-04-01 02:05:51 -0400

    Update views.py

--- app/views.py
+++ app/views.py
@@ -40,7 +40,7 @@ def signup():
 
         # Otheriswe, insert the user in the sqlite database and render wyd.html
         else:
-            newUser = User(username=form.username.data, password=form.password.data, email=form.email.data, name=form.name.data)
+            newUser = User(username=form.username.data, password=form.password.data, email=form.email.data, name=form.name.data, securityQ=form.securityQ.data, securityQanswer=form.securityQanswer.data)
             session.add(newUser)
             session.commit()
 
