aec76baef7aab8d9fb21e4154e6741d49d75a8d4
malancas <malancaster0@gmail.com>
2017-04-24 20:46:05 -0400

    Added testing Cypher queries for recs route

--- tests.py
+++ tests.py
@@ -41,15 +41,45 @@ class FlaskrTestCase(unittest.TestCase):
     def recs(self):
         # Insert test users and activities
         for i in range(0, 5):
-            graph.data("INSERT (actvy: Activity) {name:{aName}}", aName = ("testActivity" + str(i)))
+            graph.data("INSERT (actvy: Activity) {name:{aName}}", aName = ("testActivity(%d)" % i))
         
         for i in range(0, 4):
-            graph.data("INSERT (u: User) {username:{uname}}", uname = ("testUser" + str(i)))
+            graph.data("INSERT (u: User) {username:{uname}}", uname = ("testUser(%d)" % i))
 
-        # user 0 is connected to activites 0,1
-        # user 1 is connected to activities 0, 1, 2, 3, 4
-        # user 2 is connected to activities 1, 2, 3, 4
-        # user 3 is connected to activities 1, 4
+        '''
+        user 0 is connected to activites 0,1
+        user 1 is connected to activities 0, 1, 2, 3, 4
+        user 2 is connected to activities 1, 2, 3, 4
+        user 3 is connected to activities 1, 4
+        '''
+        graph.data("MATCH (u:User {username:{uname}}), (a0:Activity {name:{a0name}}), (a1:Activity {name:{a1name}})" ,
+                   "CREATE (u)-[:HAS_BEEN_TO]->(a0)",
+                   "CREATE (u)-[:HAS_BEEN_TO]->(a1)",
+                   uname = 'user0', a0name = 'testActivity0', a1name = 'testActivity1')
+
+        graph.data("MATCH (u:User {username:{uname}}), (a0:Activity {name:{a0name}}), (a1:Activity {name:{a1name}}), (a2:Activity {name:{a2name}}), (a3:Activity {name:{a3name}}), (a4:Activity {name:{a4name}})",
+                   "CREATE (u)-[:HAS_BEEN_TO]->(a0)",
+                   "CREATE (u)-[:HAS_BEEN_TO]->(a1)",
+                   "CREATE (u)-[:HAS_BEEN_TO]->(a2)",
+                   "CREATE (u)-[:HAS_BEEN_TO]->(a3)",
+                   "CREATE (u)-[:HAS_BEEN_TO]->(a4)",
+                   uname = 'user1', a0name = 'testActivity0', a1name = 'testActivity1', a2name = 'testActivity2', a3name = 'testActivity3', a4name = 'testActivity4')
+
+        graph.data("MATCH (u:User {username:{uname}}), (a1:Activity {name:{a1name}}), (a2:Activity {name:{a2name}}), (a3:Activity {name:{a3name}}), (a4:Activity {name:{a4name}})",
+                   "CREATE (u)-[:HAS_BEEN_TO]->(a1)",
+                   "CREATE (u)-[:HAS_BEEN_TO]->(a2)",
+                   "CREATE (u)-[:HAS_BEEN_TO]->(a3)",
+                   "CREATE (u)-[:HAS_BEEN_TO]->(a4)",
+                   uname = 'user2', a1name = 'testActivity1', a2name = 'testActivity2', a3name = 'testActivity3', a4name = 'testActivity4')
+
+        graph.data("MATCH (u:User {username:{uname}}), (a1:Activity {name:{a1name}}), (a4:Activity {name:{a4name}})" ,
+                   "CREATE (u)-[:HAS_BEEN_TO]->(a1)",
+                   "CREATE (u)-[:HAS_BEEN_TO]->(a4)",
+                   uname = 'user3', a1name = 'testActivity1', a4name = 'testActivity4')        
+
+MATCH (u:User {username:'admin'}), (r:Role {name:'ROLE_WEB_USER'})
+CREATE (u)-[:HAS_ROLE]->(r)
+
 
     def test_signup(self):
         # Fields are username, password, email, name, security question number, security question answer
