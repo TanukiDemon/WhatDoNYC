6989259531d1e5d8c3f490f030366a63208be29d
malancas <malancaster0@gmail.com>
2017-04-28 14:18:04 -0400

    Updated recs.html to use recommendations from recs route. Updated recs route to give the template four recommendations at a time

--- app/views.py
+++ app/views.py
@@ -224,7 +224,7 @@ def recs():
     sortedActivities = sorted(activities.items(), key=lambda x: x[1], reverse=True)
 
     form = recsForm(request.form)
-    form.recommendations.choices = sortedActivities
+    form.recommendations.choices = sortedActivities[0:4]
 
     # Pick most popular activitity and pass it along to recs.html
     return render_template('recs.html', title="Your recommendations", form=form)
