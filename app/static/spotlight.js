7935a84b76479ce288a91084e16f1041b94fad88
Dina Mangialino <dmangialino@users.noreply.github.com>
2017-05-04 15:34:23 -0400

    Added comments

--- app/static/spotlight.js
+++ app/static/spotlight.js
@@ -1,3 +1,5 @@
+// There are two different grids of images on the template index.html: gird and gridMobile
+// This function detects the size of the user's screen and hides one grid accordingly
 function hideShow() {
 	if(window.innerWidth < 750) {
 		document.getElementById("grid").style.visibility = "hidden";
@@ -5,6 +7,8 @@ function hideShow() {
 		document.getElementById("gridMobile").style.visibility = "hidden";
 	}
 }
+
+// These functions open a pop-up window of fixed height for each of the spotlight sites
 
 function spotMHP() {
 	myWindow=window.open("MHP", "_blank", "toolbar=no,scrollbars=no,resizable=no,top=100,left=300,width=800,height=650");
@@ -30,6 +34,7 @@ function spotLI() {
 	myWindow=window.open("LI", "_blank", "toolbar=no,scrollbars=no,resizable=no,top=100,left=300,width=800,height=600");
 }
 
+// This function is used for the close button on the pop-ups to close the pop-up when clicked
 function winClose() {
 	window.close();
 }
