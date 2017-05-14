11fac02c6519b8130819676fc2ac5a9ae6e9d3bd
Dina Mangialino <dmangialino@users.noreply.github.com>
2017-05-13 20:28:45 -0400

    Added function to load labels based on screen size

--- app/static/recs.js
+++ app/static/recs.js
@@ -1,12 +1,21 @@
+// Decides which labels for recommendations to show based on size of screen
+function hideShow() {
+	if(window.innerWidth > 750) {
+		document.getElementById("placeName1").style.visibility = "hidden";
+		document.getElementById("placeName2").style.visibility = "hidden";
+		document.getElementById("placeName3").style.visibility = "hidden";
+	}
+}/
+
 // Asynchronous HTTP GET request for "theUrl" parameter provided to the function
 function httpGetAsync(theUrl, callback) {
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.onreadystatechange = function() {
-  if(xmlHttp.readyState == 4 && xmlHttp.status == 200)
-    callback(xmlHttp.responseText);
-  }
-  xmlHttp.open("GET", theUrl, true);
-  xmlHttp.send(null);
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.onreadystatechange = function() {
+		if(xmlHttp.readyState == 4 && xmlHttp.status == 200)
+			callback(xmlHttp.responseText);
+	}
+	xmlHttp.open("GET", theUrl, true);
+	xmlHttp.send(null);
 }
 
 // Empty callback function utilized by httpGetAsync function
@@ -19,13 +28,13 @@ function mycallback(data) {}
 // The url is for the /feedback route in views.py which adds a relationship for the rating provided by the use between the user's node 
 //   and the node of the activity/venue that was recommended in Neo4j
 function getFeedback(rating, placeId) {
-  url = "/feedback?rating=" + rating + "&placeId=" + placeId;
-  httpGetAsync(url, mycallback);
+	url = "/feedback?rating=" + rating + "&placeId=" + placeId;
+	httpGetAsync(url, mycallback);
 }
       
 function logout() {
-  url = "/logout";
-  httpGetAsync(url, mycallback);
+	url = "/logout";
+	httpGetAsync(url, mycallback);
 }
 
 // Creates pop-up window when user clicks on recommendation
